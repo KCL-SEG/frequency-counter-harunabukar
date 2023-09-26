ff5b6da76f137c613a286da89b2ca43ebf903046
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-09-26 11:52:39 +0100

    Solution is complete and passes tests

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,18 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    stringItems = [str(item) for item in items]
+
+    for i in stringItems:
+        if i not in frequencies:
+            frequencies[str(i)] = 1
+        elif i in frequencies:
+            frequencies[str(i)] = frequencies.get(i) + 1
+            
+            
+       
+    
+
+
+
     return frequencies
